1941acaf78ffe6147f7b879feae6fa40ba84d9c0
HaleemHussain <haleemhussain64@gmail.com>
2026-03-19 13:29:42 +0000

    add url slug for products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Product extends Model
 {
     use HasFactory;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($product) {
+            $product->slug = Str::slug($product->name);
+        });
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
 }
--- resources/views/components/product-grid-item.blade.php
+++ resources/views/components/product-grid-item.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('product.show', $product) }}">
+<a href="{{ route('product.show', $product->slug) }}">
     <img src="{{ asset('images/product/' . $product->image) }}" alt="{{ $product->name }}">
     {{ $product->name }}
 </a>
